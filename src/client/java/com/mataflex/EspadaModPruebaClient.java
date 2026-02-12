292e057fd3d782a2b5856c746fc5f05ce6d53062
sebas <sebasmata09@gmail.com>
2026-02-12 12:54:10 -0600

    Initial commit

---
+++ src/client/java/com/mataflex/EspadaModPruebaClient.java
@@ -0,0 +1,10 @@
+package com.mataflex;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EspadaModPruebaClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
